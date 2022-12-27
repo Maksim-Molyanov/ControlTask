c4c0f8dfcdfb14fdbcbd9bbab25a81a065fda1e6
Maksim Molyanov <fioklit@gmail.com>
2022-12-27 22:05:33 +0400

    Added function filter array

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,8 +1,34 @@
-﻿int size = int.Parse(Console.ReadLine());
+﻿Console.Write("Введите длину массива: ");
+int size = int.Parse(Console.ReadLine());
 
-
+Console.WriteLine("Заполните массив: ");
 string[] array = CreateArr(size);
+Console.WriteLine();
+Console.WriteLine("Исходный массив: ");
 PrintArr(array);
+
+string[] result = FilterArr(array);
+Console.WriteLine();
+Console.WriteLine("Отфильтрованный массив: ");
+PrintArr(result);
+
+string[] FilterArr(string[] array)
+{
+    string[] filteredArr = new string [array.Length];
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            filteredArr[j] = array[i];
+            j++;
+        }
+    }
+    Array.Resize(ref filteredArr, j);
+    return filteredArr;
+}
+
+
 
 string[] CreateArr(int size)
 {
@@ -20,4 +46,5 @@ void PrintArr(string[] array)
     {
         Console.Write($"{array[i]}, ");
     }
+    Console.WriteLine();
 } 
