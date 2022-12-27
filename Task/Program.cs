08f9d74985745f9a52224a7d3339eebf1e6da5ef
Maksim Molyanov <fioklit@gmail.com>
2022-12-27 21:21:28 +0400

    Created functions create and print array

---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+
+string[] array = CreateArr(size);
+PrintArr(array);
+
+string[] CreateArr(int size)
+{
+    string[] array = new string [size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array; 
+}
+
+void PrintArr(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+} 
